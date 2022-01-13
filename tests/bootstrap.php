2debe16b61e4033c35b91a0b6b38c987015b1df7
Nauzet Plasencia Cruz <nauzet.plasencia.cruz@gmail.com>
2022-01-13 05:25:47 +0000

    Enable DAMADoctrineTestBundle to run on tests. Remove test cache when debug mode is disabled.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,20 @@
-<?php
+<?php declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__).'/vendor/autoload.php';
+
+ensureFreshCacheWhenDebugModeIsDisabled();
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+function ensureFreshCacheWhenDebugModeIsDisabled(): void
+{
+    $filesystem = new Filesystem();
+    $filesystem->remove(__DIR__.'/../var/cache/test');
 }
